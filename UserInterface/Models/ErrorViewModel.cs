7c91d85562adcaba644ff71404a28b3e4ad2202c
Jantje <jtb.vanberkel@student.avans.nl>
2023-09-26 14:01:35 +0200

    Add project files.

---
+++ UserInterface/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace UserInterface.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
