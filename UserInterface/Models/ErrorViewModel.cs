3ceebd8855bdb78da010e2bf0d8849bf55fb9cc7
Jantje <jtb.vanberkel@student.avans.nl>
2023-09-27 09:31:44 +0200

    Added Infrastructure

--- UserInterface/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace UserInterface.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
