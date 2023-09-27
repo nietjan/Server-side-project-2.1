a92e9c887e61973969e0fba804b4597768d84e24
Jantje <jtb.vanberkel@student.avans.nl>
2023-09-27 10:25:01 +0200

    Added iRepository and inMemoryRepository implementation of iRepository

---
+++ DomainServices/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel;
+
+namespace DomainServices {
+    public interface IRepository {
+        public IEnumerable<Packet> GetPackets();
+        public IEnumerable<Packet> GetSinglePacket(int id);
+        public Task<bool> addPacket(Packet packet);
+        public IEnumerable<Product> GetAxampleProducts(int id);
+        public Task<bool> reservePacket(int packetId, string personEmail);
+    }
+}
---
+++ Infrastructure/InMemoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel;
+using DomainServices;
+
+namespace Infrastructure {
+    public class InMemoryRepository : IRepository {
+        private static List<Packet> packets = new List<Packet>();
+        public async Task<bool> addPacket(Packet packet) {
+            packets.Add(packet);
+            return true;
+        }
+
+        public IEnumerable<Product> GetAxampleProducts(int id) {
+            return packets.ElementAt(id).axampleProducts;
+        }
+
+        public IEnumerable<Packet> GetPackets() {
+            return packets;
+        }
+
+        public IEnumerable<Packet> GetSinglePacket(int id) {
+            return packets.Where(i => i.id == id);
+        }
+
+        public async Task<bool> reservePacket(int packetId, string personEmail) {
+            packets.ElementAt(packetId).reservedBy = personEmail;
+            return true;
+        }
+    }
+}
